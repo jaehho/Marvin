4fee6125a4439cdb7a3679123903efed86cdf7b4
Jaeho Cho <jaeho.cho@cooper.edu>
2024-02-28 20:27:13 -0500

    update setup.py to add stl files in share directory

--- ros2_ws/src/marvin/setup.py
+++ ros2_ws/src/marvin/setup.py
@@ -12,7 +12,9 @@ setup(
         ('share/ament_index/resource_index/packages',
             ['resource/' + package_name]),
         ('share/' + package_name, ['package.xml']),
-        (os.path.join('share', package_name, 'launch'), glob(os.path.join('launch', '*launch.[pxy][yma]*')))
+        (os.path.join('share', package_name, 'launch'), glob(os.path.join('launch', '*launch.[pxy][yma]*'))),
+        # Add the URDF meshes directory
+        (os.path.join('share', package_name, 'urdf', 'meshes'), glob('urdf/meshes/*'))
     ],
     install_requires=['setuptools'],
     zip_safe=True,
