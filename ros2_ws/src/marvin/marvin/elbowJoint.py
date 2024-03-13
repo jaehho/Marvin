b092911d4b7827b83ed3353f82cf957020b8f10e
Jaeho Cho <jaeho.cho@cooper.edu>
2024-03-13 18:30:33 -0400

    using vector module

--- ros2_ws/src/marvin/marvin/elbowJoint.py
+++ ros2_ws/src/marvin/marvin/elbowJoint.py
@@ -3,19 +3,7 @@ from rclpy.node import Node
 from custom_interfaces.msg import PoseLandmark
 import numpy as np
 from std_msgs.msg import Float64  # Import Float64 message type
-
-def vector_from_points(p1, p2):
-    """Create a vector from two points."""
-    return np.array([p2.x - p1.x, p2.y - p1.y, p2.z - p1.z])
-
-def calculate_angle(v1, v2):
-    """Calculate the angle between two vectors."""
-    dot_product = np.dot(v1, v2)
-    magnitude_product = np.linalg.norm(v1) * np.linalg.norm(v2)
-    cos_angle = dot_product / magnitude_product
-    angle_radians = np.arccos(cos_angle)
-    angle_degrees = np.degrees(angle_radians)
-    return angle_degrees
+from .vector import vector_from_points, calculate_angle
 
 class ElbowAngleSubscriber(Node):
 
