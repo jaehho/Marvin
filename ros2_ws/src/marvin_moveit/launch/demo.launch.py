5356072c726fd085daa2ee6426c8b20df23c24aa
Jaeho Cho <jaeho2025@gmail.com>
2024-11-12 22:45:56 -0500

    pre moveit package

--- ros2_ws/src/marvin_moveit/launch/demo.launch.py
+++ ros2_ws/src/marvin_moveit/launch/demo.launch.py
@@ -3,5 +3,8 @@ from moveit_configs_utils.launches import generate_demo_launch
 
 
 def generate_launch_description():
-    moveit_config = MoveItConfigsBuilder("marvin", package_name="marvin_moveit").to_moveit_configs()
+    #moveit_config = MoveItConfigsBuilder("marvin", package_name="marvin_moveit").to_moveit_configs()
+    moveit_config = (
+        MoveItConfigsBuilder("marvin").robot_description("ros2_ws/src/marvin/urdf/marvin.urdf").robot_description_semantic("ros2_ws/src/marvin_moveit/config/marvin.srdf").trajectory_executiion("ros2_ws/src/marvin_moveit/config/joint_limits.yaml").to_moveit_configs()
+    )
     return generate_demo_launch(moveit_config)
