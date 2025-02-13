82e39f10dd9d807d1c637065783a600b4c856d0f
Jaeho Cho <jaeho2025@gmail.com>
2025-02-13 02:30:53 -0500

    control.vue to typescript

--- app/pages/control.vue
+++ app/pages/control.vue
@@ -4,12 +4,13 @@
   </div>
 </template>
 
-<script>
+<script lang="ts">
+import { defineComponent } from 'vue';
 import VideoChat from "~/components/VideoChat.vue";
 
-export default {
+export default defineComponent({
   components: {
     VideoChat,
   },
-};
+});
 </script>
