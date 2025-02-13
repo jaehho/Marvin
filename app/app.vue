986be2b724f23588ade7ad9b21cd96263292c244
Jaeho Cho <jaeho2025@gmail.com>
2025-02-12 23:27:30 -0500

    experiment

--- app/app.vue
+++ app/app.vue
@@ -1,6 +1,27 @@
+<script>
+import 'animate.css'
+</script>
+
+<style>
+@import url("animate.css");
+</style>
+
 <template>
+  <header>
+    <nav>
+      <ul>
+        <li><NuxtLink to="/">Home</NuxtLink></li>
+        <li><NuxtLink to="/about">About</NuxtLink></li>
+      </ul>
+    </nav>
+  </header>
   <div>
-    <NuxtRouteAnnouncer />
-    <NuxtWelcome />
+    <h1>This is app.vue</h1>
+    <AppAlert>
+      This is an auto-imported component from app.vue
+    </AppAlert>
+    <NuxtPage />
+    <img src="/marvin.jpg" alt="marvin" style="width: 30%; height: auto;" />
+    <h1 class="animate__animated animate__bounce">An animated element</h1>
   </div>
 </template>
---
+++ app/components/AppAlert.vue
@@ -0,0 +1,6 @@
+<template>
+    yuh im from components <br>
+    <span>
+        <slot />
+    </span>
+</template>
