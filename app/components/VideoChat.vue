9e67ea881f2c5133c086aad37973cc3e71ef90a9
Jaeho Cho <jaeho2025@gmail.com>
2025-02-13 02:29:29 -0500

    usePoseDetection to typescript

--- app/components/VideoChat.vue
+++ app/components/VideoChat.vue
@@ -36,7 +36,6 @@
 <script lang="ts">
 import { defineComponent } from "vue";
 import Peer from "peerjs";
-import { usePoseDetection } from "~/composables/usePoseDetection";
 
 export default defineComponent({
   name: "VideoChat",
