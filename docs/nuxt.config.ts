c08bdba126aa32a48bf46ba2520835492694c63e
Jaeho Cho <jaeho2025@gmail.com>
2025-03-05 14:13:37 -0500

    gh pages prep

--- docs/nuxt.config.ts
+++ docs/nuxt.config.ts
@@ -3,4 +3,8 @@ export default defineNuxtConfig({
   ssr: false, // Disable server-side rendering if not needed
   compatibilityDate: "2024-11-01",
   devtools: { enabled: true },
+  target: 'static', // Enables static site generation
+  app: {
+    baseURL: '/marvin/', // Replace <REPO_NAME> with your repository name
+  },
 });
